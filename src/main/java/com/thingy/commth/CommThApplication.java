48c38e452ed853ad6f6796f4a7234e40c56ce72e
Ord1naryMan <Ord1naryMan.dmitry@gmail.com>
2023-08-18 12:44:46 +0300

    Initial commit

---
+++ src/main/java/com/thingy/commth/CommThApplication.java
@@ -0,0 +1,13 @@
+package com.thingy.commth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommThApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thingy/commth/CommThApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thingy.commth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommThApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
